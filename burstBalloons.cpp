b8c05231e9d837ddf834088a7b0392777efde4a6
argon17 <premsagark.ug18.ee@nitp.ac.in>
2022-01-01 16:55:05 +0530

    01/01/2022

--- burstBalloons.cpp
+++ burstBalloons.cpp
@@ -1,4 +1,4 @@
-// O(N!) not AC :(
+// O(N!)    23/70 TC passed :(
 class Solution {
 public:
     int maxCoins(vector<int>& a) {
@@ -17,5 +17,32 @@ public:
             mx = max(mx, here);
         }
         return mx;
+    }
+};
+
+// 70/70 test case passed
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        memo.resize(n + 1, vector<int>(n + 1, -1));
+        nums.insert(begin(nums), 1);
+        nums.push_back(1);
+        return burstRange(nums, 1, n);
+    }
+private:
+    vector<vector<int>> memo;
+    int burstRange(vector<int> &a, int l, int r){
+        if(r < l) return 0;
+        if(memo[l][r] != -1) return memo[l][r];
+        int ans = 0;
+        // assuming p'th index is the last balloon burst in this range
+        for(int p = l; p <= r; ++p){
+            int here = a[l - 1] * a[p] * a[r + 1];
+            here += burstRange(a, l, p - 1) + burstRange(a, p + 1, r);
+            ans = max(ans, here);
+        }
+        return memo[l][r] = ans;
     }
 };
