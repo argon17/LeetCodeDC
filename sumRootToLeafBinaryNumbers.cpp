456a08665d44aeb1c851cc02ffb531a898a3762d
argon17 <premsagark.ug18.ee@nitp.ac.in>
2022-01-12 08:19:58 +0530

    11/01/2022

--- sumRootToLeafBinaryNumbers.cpp
+++ sumRootToLeafBinaryNumbers.cpp
@@ -3,6 +3,8 @@ public:
         int sumRootToLeaf(TreeNode* root, int val = 0) {
             if (!root) return 0;
             val = (val * 2 + root->val);
+            // root->left == root->right => A very nice way to check if it's a leaf node.
+            // if it's a leaf node, no recursive call further.
             return root->left == root->right ? val : sumRootToLeaf(root->left, val) + sumRootToLeaf(root->right, val);
     }
 };
