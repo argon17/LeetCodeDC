e5b7c774e77b7fc96eb2c6205c1e12aba9b3e6fe
argon17 <premsagark.ug18.ee@nitp.ac.in>
2022-02-03 20:18:06 +0530

    03/03/2022

---
+++ 4sum2.py
@@ -0,0 +1,4 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        store = Counter([num1 + num2 for num1 in nums1 for num2 in nums2])
+        return sum([store[-num3 -num4] for num3 in nums3 for num4 in nums4])
