51625b4677d1a414474c1d7781e1aaf56a971611
argon17 <premsagark.ug18.ee@nitp.ac.in>
2022-02-07 08:24:05 +0530

    07/02/2022

---
+++ findTheDifference.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        ans = 0
+        for char in s + t:
+            ans ^= ord(char)
+        return chr(ans)
+        
