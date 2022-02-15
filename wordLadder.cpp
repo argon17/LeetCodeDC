929374d6736023e0c18e9b51e45796484aed98ab
argon17 <premsagark.ug18.ee@nitp.ac.in>
2022-02-15 19:31:38 +0530

    12/02/2022

--- wordLadder.cpp
+++ wordLadder.cpp
@@ -26,3 +26,41 @@ public:
         return 0;
     }
 };
+
+// Optimized !!
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(find(begin(wordList), end(wordList), endWord) == end(wordList)) return 0;
+        // string : [string]
+        map<string, vector<string>> adj;
+        for(string word : wordList){
+            for(int ind = 0; ind < word.size(); ++ind) {
+                string pat = word;
+                pat[ind] = '*';
+                adj[pat].push_back(word);
+            }
+        }
+        queue<string> q;
+        set<string> vis;
+        q.push(beginWord);
+        vis.insert(beginWord);
+        for(int ans = 1; q.size(); ++ans){
+            int sz = q.size();
+            while(sz--){
+                string cur = q.front();
+                q.pop();
+                if(cur == endWord) return ans;
+                for(int ind = 0; ind < cur.size(); ++ind){
+                    string pat = cur;
+                    pat[ind] = '*';
+                    for(string nbr : adj[pat])
+                        if(vis.find(nbr) == vis.end()){
+                            q.push(nbr);
+                            vis.insert(nbr);
+                }
+            }
+        }
+        return 0;
+    }
+};
